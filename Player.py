a715ca78f1eec37f90c30ffb445141f2dfc346ed
TheCoolestMango <thecoolestmango@gmail.com>
2022-11-15 20:25:07 +0300

    add genre choice in a choicebox, replace buttons

--- Player.py
+++ Player.py
@@ -2,6 +2,7 @@
 
 import os
 from tkinter import *
+from tkinter import ttk
 from tkinter.ttk import *
 from tkinter import Tk
 from tkinter import filedialog
@@ -11,69 +12,93 @@ from PIL import Image, ImageTk
 # Create a GUI window
 root = Tk()
 root.title("Music Player")
-root.geometry("920x600+290+85")
+root.geometry("720x600+290+85")
 root.configure(background='#212121')
 root.resizable(False, False)
 mixer.init()
-
-# Create a function to open a file
-def AddMusic():
-    path = filedialog.askdirectory()
-    if path:
-        os.chdir(path)
-        songs = os.listdir(path)
-
-        for song in songs:
-            if song.endswith(".wav"):
-                Playlist.insert(END, song)
 
 
 def PlayMusic():
     Music_Name = Playlist.get(ACTIVE)
     print(Music_Name[0:-4])
-    mixer.music.load(Playlist.get(ACTIVE))
+    mixer.music.load(Music_Name)
     mixer.music.play()
 
+icons_folder = '/home/liza/Study/pmldl/Project/images/'
+
 # icon
-image_icon = PhotoImage(file="icon3.png")
+image_icon = PhotoImage(file=icons_folder+"icon3.png")
 root.iconphoto(False, image_icon)
 
-Top = PhotoImage(file="top_image.png")
+Top = PhotoImage(file=icons_folder+"top_image.png")
 Label(root, image=Top, background="#0f1a2b").pack()
 
 # logo
-logo = PhotoImage(file="logo.png")
-Label(root, image=logo, background="#0f1a2b").place(x=70, y=115)
+logo = PhotoImage(file=icons_folder+"logo.png")
+Label(root, image=logo, background="#0f1a2b").place(x=45, y=150)
+
+# background for buttons
+Frame_buttons = Frame(root, relief=RIDGE)
+Frame_buttons.place(x=45, y=350, width=202, height=150)
 
 # Button
-ButtonPlay = PhotoImage(file="play.png")
+ButtonPlay = PhotoImage(file=icons_folder+"play.png")
 Button(root, image=ButtonPlay,
-       command=PlayMusic).place(x=100, y=400)
+       command=PlayMusic).place(x=115, y=360)
 
-ButtonStop = PhotoImage(file="stop.png")
+ButtonStop = PhotoImage(file=icons_folder+"stop.png")
 Button(root, image=ButtonStop,
-       command=mixer.music.stop).place(x=30, y=500)
+       command=mixer.music.stop).place(x=50, y=425)
 
-ButtonResume = PhotoImage(file="resume.png")
+ButtonResume = PhotoImage(file=icons_folder+"resume.png")
 Button(root, image=ButtonResume,
-       command=mixer.music.unpause).place(x=115, y=500)
+       command=mixer.music.unpause).place(x=115, y=425)
 
-ButtonPause = PhotoImage(file="pause.png")
+ButtonPause = PhotoImage(file=icons_folder+"pause.png")
 Button(root, image=ButtonPause,
-       command=mixer.music.pause).place(x=200, y=500)
+       command=mixer.music.pause).place(x=180, y=425)
 
 
 # Label 
-Menu = PhotoImage(file="menu.png")
+Menu = PhotoImage(file=icons_folder+"menu.png")
 Label(root, image=Menu).pack(padx=10, pady=50, side=RIGHT)
 
 Frame_Music = Frame(root, relief=RIDGE)
-Frame_Music.place(x=330, y=350, width=560, height=250)
+Frame_Music.place(x=330, y=350, width=180, height=250)
 
-Button(root, text="Open Folder", width=15, command=AddMusic).place(x=330, y=300)
+
+CURR_GENRE = 'blues'
+#Button(root, text="Open Folder", width=15, command=AddMusic).place(x=330, y=300)
+
+# Label for genre choice
+label_genre = Label(text='Select genre:')
+label_genre.place(x=331, y=280)
+
+# genre choice
+genres = ['blues', 'country', 'classical', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+selected_genre = StringVar(root)
+genre_choice = Combobox(root, values=genres, textvariable=selected_genre)
+genre_choice.place(x=330, y=300)
+# prevent typing a value
+genre_choice['state'] = 'readonly'
+
+# update current genre on genre change
+def genre_changed(event):
+       curr_genre = selected_genre.get()
+       Playlist.delete(0, END)
+       path = '/home/liza/Music/genres_original/'+curr_genre
+       if path:
+              os.chdir(path)
+              songs = os.listdir(path)
+
+       for song in songs:
+              if song.endswith(".wav"):
+                     Playlist.insert(END, song)
+
+genre_choice.bind('<<ComboboxSelected>>', genre_changed)
 
 Scroll = Scrollbar(Frame_Music)
-Playlist = Listbox(Frame_Music, width=100, font=("Times new roman", 10), background="#333333", fg="grey", selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
+Playlist = Listbox(Frame_Music, width=100, font=("Calibri", 10), background="#333333", fg="grey", selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
 Scroll.config(command=Playlist.yview)
 Scroll.pack(side=RIGHT, fill=Y)
 Playlist.pack(side=LEFT, fill=BOTH)
